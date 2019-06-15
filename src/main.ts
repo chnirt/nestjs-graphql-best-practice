a4fed22819169d14e57fe375b7a2086fc7419068
Trinh Chin <trinhchin@Trinhs-MacBook-Pro.local>
2019-06-16 04:02:54 +0700

    CRUD

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+
+const port = process.env.PORT || 3000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(port);
+  Logger.log(`🚀 Server running on http://localhost:${port} `, 'Bootstrap');
 }
 bootstrap();
