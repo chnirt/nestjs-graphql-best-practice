4535ccd2909856792979b39eb4a21e02b8a78e6a
Trinh Chin <trinhchin@Trinhs-MacBook-Pro.local>
2019-06-23 18:37:33 +0700

    Custom Webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,56 +1,8 @@
-// const webpack = require('webpack');
-// const path = require('path');
-// const nodeExternals = require('webpack-node-externals');
-// const chalk = require('chalk');
-// const ProgressBarPlugin = require('progress-bar-webpack-plugin');
-
-// module.exports = {
-//   entry: ['webpack/hot/poll?100', './src/main.ts'],
-//   watch: true,
-//   target: 'node',
-//   externals: [
-//     nodeExternals({
-//       whitelist: ['webpack/hot/poll?100'],
-//     }),
-//   ],
-//   module: {
-//     rules: [
-//       {
-//         test: /.tsx?$/,
-//         use: 'ts-loader',
-//         exclude: /node_modules/,
-//       },
-//     ],
-//   },
-//   mode: 'development',
-//   resolve: {
-//     extensions: ['.tsx', '.ts', '.js'],
-//   },
-//   plugins: [
-//     new webpack.HotModuleReplacementPlugin(),
-//     new ProgressBarPlugin({
-//       format:
-//         chalk.hex('#6c5ce7')('build ') +
-//         chalk.hex('#0984e3')('▯:bar▯ ') +
-//         // chalk.red('▯ :bar ▯ ') +
-//         chalk.hex('#00b894')('(:percent) ') +
-//         // chalk.green(':percent ') +
-//         chalk.hex('#ffeaa7')(':msg'),
-//       // chalk.blue('( :elapsed s )')
-//       complete: '▰',
-//       incomplete: '▱',
-//       clear: false,
-//     }),
-//   ],
-//   output: {
-//     path: path.join(__dirname, 'dist'),
-//     filename: 'server.js',
-//   },
-// };
-
 const webpack = require('webpack');
 const path = require('path');
 const nodeExternals = require('webpack-node-externals');
+const chalk = require('chalk');
+const ProgressBarPlugin = require('progress-bar-webpack-plugin');
 
 module.exports = {
   entry: ['webpack/hot/poll?100', './src/main.ts'],
@@ -64,7 +16,7 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.tsx?$/,
+        test: /.tsx?$/,
         use: 'ts-loader',
         exclude: /node_modules/,
       },
@@ -76,7 +28,19 @@ module.exports = {
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
-    new webpack.WatchIgnorePlugin([/\.js$/, /\.d\.ts$/]),
+    new ProgressBarPlugin({
+      format:
+        chalk.hex('#6c5ce7')('build ') +
+        chalk.hex('#0984e3')('▯:bar▯ ') +
+        // chalk.red('▯ :bar ▯ ') +
+        chalk.hex('#00b894')('(:percent) ') +
+        // chalk.green(':percent ') +
+        chalk.hex('#ffeaa7')(':msg'),
+      // chalk.blue('( :elapsed s )')
+      complete: '▰',
+      incomplete: '▱',
+      clear: false,
+    }),
   ],
   output: {
     path: path.join(__dirname, 'dist'),
