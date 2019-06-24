7e9daf3ff841d9bf8b172341839c6633ae23d006
Trinh Chin <trinhchin@Trinhs-MacBook-Pro.local>
2019-06-24 12:21:46 +0700

    Fix

---
+++ src/common/interceptors/http-cache.interceptor.ts
@@ -0,0 +1,19 @@
+import { CacheInterceptor, ExecutionContext, Injectable } from '@nestjs/common';
+
+@Injectable()
+class HttpCacheInterceptor extends CacheInterceptor {
+  trackBy(context: ExecutionContext): string | undefined {
+    const request = context.switchToHttp().getRequest();
+    const httpServer = request.applicationRef;
+
+    const isGetRequest = httpServer.getRequestMethod(request) === 'GET';
+    const excludePaths = [];
+    if (
+      !isGetRequest ||
+      (isGetRequest && excludePaths.includes(httpServer.getRequestUrl))
+    ) {
+      return undefined;
+    }
+    return httpServer.getRequestUrl(request);
+  }
+}
--- src/common/middleware/logger.middleware.ts
+++ src/common/middleware/logger.middleware.ts
@@ -3,7 +3,7 @@ import { Injectable, NestMiddleware } from '@nestjs/common';
 @Injectable()
 export class LoggerMiddleware implements NestMiddleware {
   use(req: any, res: any, next: () => void) {
-    console.log('Logger...');
+    console.log(`Request...`);
     // console.log(`[${context}] Request...`);
     next();
   }
