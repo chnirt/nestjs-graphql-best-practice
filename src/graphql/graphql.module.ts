40f1307db8211e0aedb3b5cdeaeeab9f7547942b
Trinh Chin <trinhchin@Trinhs-MacBook-Pro.local>
2019-06-25 17:09:35 +0700

    Fix All

--- src/graphql/graphql.module.ts
+++ src/graphql/graphql.module.ts
@@ -1,11 +1,12 @@
-import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from '../user/user.entity';
-import { GraphqlService } from './graphql.service';
-import { UserService } from '../user/user.service';
+import { Module } from '@nestjs/common'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { User } from '../user/user.entity'
+import { GraphqlService } from './graphql.service'
+import { UserService } from '../user/user.service'
+import { UserModule } from '../user/user.module'
 
 @Module({
-  // imports: [TypeOrmModule.forFeature([User])],
-  providers: [GraphqlService],
+	// imports: [TypeOrmModule.forFeature([User]), UserModule],
+	providers: [GraphqlService]
 })
 export class GraphqlModule {}
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,13 +1,13 @@
-import { Module, Global } from '@nestjs/common';
-import { UserResolver } from './user.resolver';
-import { UserService } from './user.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from './user.entity';
+import { Module, Global } from '@nestjs/common'
+import { UserResolver } from './user.resolver'
+import { UserService } from './user.service'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { User } from './user.entity'
 
 @Global()
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
-  providers: [UserResolver, UserService],
-  exports: [UserService],
+	imports: [TypeOrmModule.forFeature([User])],
+	providers: [UserResolver, UserService],
+	exports: [UserService]
 })
 export class UserModule {}
