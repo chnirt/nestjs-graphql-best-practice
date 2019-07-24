8ae5d504e2ebe31f4ee3e02d6b341f04c99db2cf
chintrinh <trinhchinchin@gmail.com>
2019-07-24 13:42:06 +0700

    FIX Package.json

--- src/config.env.ts
+++ src/config.env.ts
@@ -1,9 +1,11 @@
 const config = {
 	development: {
-		port: 11048
+		port: 11048,
+		end_point: 'graphql'
 	},
 	production: {
-		port: 11047
+		port: process.env.PORT,
+		end_point: process.env.END_POINT
 	}
 }
 export default config[process.env.NODE_ENV || 'development']
