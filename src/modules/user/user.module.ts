8435bc420256db1327ae8ebd206d8deecf82110d
hieutran111 <minhhieutran11197@gmail.com>
2019-08-12 10:09:09 +0700

    fix tslint config

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common'
 import { UserResolver } from './user.resolver'
-import { UserService } from '../../../../lunchapp-innos-backend/src/modules/user/user.service'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { User } from './user.entity'
 
 @Module({
 	imports: [TypeOrmModule.forFeature([User])],
-	providers: [UserResolver, UserService]
+	providers: [UserResolver]
 })
 export class UserModule {}
