45c1d5e4a215accc4318acc313ae7e3fbb4dc8bd
chintrinh <trinhchinchin@gmail.com>
2019-08-23 18:18:35 +0700

    FIX upload

--- src/utils/upload/upload.resolver.ts
+++ src/utils/upload/upload.resolver.ts
@@ -1,10 +1,17 @@
-import { Resolver, Mutation, Args } from '@nestjs/graphql'
+import { Resolver, Mutation, Args, Context } from '@nestjs/graphql'
+import { createWriteStream } from 'fs'
 
 @Resolver('Upload')
 export class UploadResolver {
 	@Mutation()
 	async singleUpload(@Args('file') file: any) {
-		console.log(file)
-		return 'nice'
+		const { filename, createReadStream } = await file
+
+		return new Promise(async (resolve, reject) =>
+			createReadStream()
+				.pipe(createWriteStream(`photos/${filename}`))
+				.on('finish', () => resolve(true))
+				.on('error', () => reject(false))
+		)
 	}
 }
