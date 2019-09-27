03e551e82f597a362714d4a9cfc274d79d79e3ec
chintrinh <trinhchinchin@gmail.com>
2019-09-27 09:32:04 +0700

    COMPLETE Cron

--- src/utils/tasks/tasks.service.ts
+++ src/utils/tasks/tasks.service.ts
@@ -1,22 +1,23 @@
-import { Injectable } from '@nestjs/common';
-const cron = require('cron');
+import { Injectable } from '@nestjs/common'
 
 @Injectable()
 export class TasksService {
-  async Timeout() {
-    const taskID = setTimeout(() => console.log('Task completed'), 2000);
-    // clearTimeout(taskID);
-  }
+	async Timeout() {
+		const taskID = setTimeout(() => console.log('Task completed'), 2000)
+		// clearTimeout(taskID);
+	}
 
-  async Interval() {
-    const intervalID = setInterval(() => console.log('Task executed'), 2000);
-    // clearInterval(intervalID);
-  }
+	async Interval() {
+		const intervalID = setInterval(() => console.log('Task executed'), 2000)
+		// clearInterval(intervalID);
+	}
 
-  async Cron() {
-    const cronJob = cron.job('*/1 * * * * *', () => {
-      console.info('Cron job completed');
-    });
-    cronJob.start();
-  }
+	async Cron() {
+		const cron = require('cron')
+
+		const cronJob = cron.job('* * 15 * * MON-FRI', () => {
+			console.info('Cron job completed')
+		})
+		cronJob.start()
+	}
 }
