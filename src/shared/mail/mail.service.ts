b606d0433737319fabe25d836939efe11f363126
chintrinh <trinhchinchin@gmail.com>
2019-10-11 12:20:57 +0700

    FIX email

--- src/shared/mail/mail.service.ts
+++ src/shared/mail/mail.service.ts
@@ -6,10 +6,12 @@ import { User } from '../../generator/graphql.schema'
 
 import { AUTHOR, ISSUER, MAIL_USER, MAIL_PASS } from '../../environments'
 
+type Type = 'verifyEmail' | 'forgotPassword'
+
 @Injectable()
 export class MailService {
 	async sendMail(
-		type: string,
+		type: Type,
 		user: User,
 		req: any,
 		token: string
@@ -35,44 +37,35 @@ export class MailService {
 		readHTMLFile('./src/assets/templates/udacity-index.html', (err, html) => {
 			const template = handlebars.compile(html)
 
+			const common = {
+				author: AUTHOR!,
+				issuer: ISSUER!,
+				ios: 'https://itunes.apple.com/us/app/chnirt',
+				android: 'https://play.google.com/store/apps/chnirt',
+				twitter: 'https://twitter.com/chnirt',
+				facebook: 'https://www.facebook.com/trinhchinchinn',
+				googleplus: 'https://plus.google.com/chnirt',
+				linkedin:
+					'https://www.linkedin.com/authwall?trk=gf&trkInfo=AQFSlEdMz0wy8AAAAW2cEMIYqabj7d0O-w7EMMY5W1BFRDacs5fcAbu4akPG8jrJQPG5-cNbLf-kaBHIfmW-f6a3WgaqAEjIG6reC_mLvY9n-mzZwZbcFf0q9XmrlkFVdVUH2I4=&originalReferer=https://www.facebook.com/&sessionRedirect=https%3A%2F%2Fwww.linkedin.com%2Fin%2Fchin-tr%25E1%25BB%258Bnh-62200215a%3Ffbclid%3DIwAR289POrXez8UY6k2RQNEnNAjrtOto8H6zhFABlQ7HHCvpIS0afgQHxGGic',
+				number: '1803',
+				street: 'Su Van Hanh',
+				city: 'Ho Chi Minh',
+				country: 'Viet Nam',
+				to: `${user.firstName}`
+			}
+
 			const replacements = {
 				verifyEmail: {
 					link: `http://${req.headers.host}/verify/${token}`,
-					author: AUTHOR!,
-					issuer: ISSUER!,
-					ios: 'https://itunes.apple.com/us/app/chnirt',
-					android: 'https://play.google.com/store/apps/chnirt',
-					twitter: 'https://twitter.com/chnirt',
-					facebook: 'https://www.facebook.com/trinhchinchinn',
-					googleplus: 'https://plus.google.com/chnirt',
-					linkedin:
-						'https://www.linkedin.com/authwall?trk=gf&trkInfo=AQFSlEdMz0wy8AAAAW2cEMIYqabj7d0O-w7EMMY5W1BFRDacs5fcAbu4akPG8jrJQPG5-cNbLf-kaBHIfmW-f6a3WgaqAEjIG6reC_mLvY9n-mzZwZbcFf0q9XmrlkFVdVUH2I4=&originalReferer=https://www.facebook.com/&sessionRedirect=https%3A%2F%2Fwww.linkedin.com%2Fin%2Fchin-tr%25E1%25BB%258Bnh-62200215a%3Ffbclid%3DIwAR289POrXez8UY6k2RQNEnNAjrtOto8H6zhFABlQ7HHCvpIS0afgQHxGGic',
-					number: '1803',
-					street: 'Su Van Hanh',
-					city: 'Ho Chi Minh',
-					country: 'Viet Nam',
-					to: `${user.firstName}`,
 					subject: 'Verify Email',
 					text1: 'To complete your sign up, please verify your email: ',
 					button: 'VERIFY EMAIL',
-					text2: 'Or copy this link and paste in your web	browser'
+					text2: 'Or copy this link and paste in your web	browser',
+					...common
 				},
 				forgotPassword: {
 					link: `http://${req.headers.host}/reset/${token}`,
-					author: AUTHOR!,
-					issuer: ISSUER!,
-					ios: 'https://itunes.apple.com/us/app/chnirt',
-					android: 'https://play.google.com/store/apps/chnirt',
-					twitter: 'https://twitter.com/chnirt',
-					facebook: 'https://www.facebook.com/trinhchinchinn',
-					googleplus: 'https://plus.google.com/chnirt',
-					linkedin:
-						'https://www.linkedin.com/authwall?trk=gf&trkInfo=AQFSlEdMz0wy8AAAAW2cEMIYqabj7d0O-w7EMMY5W1BFRDacs5fcAbu4akPG8jrJQPG5-cNbLf-kaBHIfmW-f6a3WgaqAEjIG6reC_mLvY9n-mzZwZbcFf0q9XmrlkFVdVUH2I4=&originalReferer=https://www.facebook.com/&sessionRedirect=https%3A%2F%2Fwww.linkedin.com%2Fin%2Fchin-tr%25E1%25BB%258Bnh-62200215a%3Ffbclid%3DIwAR289POrXez8UY6k2RQNEnNAjrtOto8H6zhFABlQ7HHCvpIS0afgQHxGGic',
-					number: '1803',
-					street: 'Su Van Hanh',
-					city: 'Ho Chi Minh',
-					country: 'Viet Nam',
-					to: `${user.firstName}`,
+
 					subject: 'Reset Your Password',
 					text1:
 						// tslint:disable-next-line:quotemark
@@ -80,7 +73,8 @@ export class MailService {
 					button: 'Set New Password',
 					text2:
 						// tslint:disable-next-line:quotemark
-						"If that doesn't work, copy and paste the following link in your browser:"
+						"If that doesn't work, copy and paste the following link in your browser:",
+					...common
 				}
 			}
 
