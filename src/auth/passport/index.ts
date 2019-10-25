66308b60a331ee34ab65e73319fc0a2921a18de4
chintrinh <trinhchinchin@gmail.com>
2019-10-25 17:44:26 +0700

    FIX

--- src/auth/passport/index.ts
+++ src/auth/passport/index.ts
@@ -1,24 +1,50 @@
 import * as passport from 'passport'
 import * as GooglePlusStrategy from 'passport-google-plus-token'
+import { User } from '../../models'
 
 import {
 	GOOGLE_CLIENT_ID,
 	GOOGLE_CLIENT_SECRET,
 	GOOGLE_CALLBACK_URL
 } from '../../environments'
+import { getMongoRepository } from 'typeorm'
+import { AuthenticationError } from 'apollo-server-core'
 
 // GOOGLE PLUS STRATEGY
-const GooglePlusTokenStrategyCallback = (
+const GooglePlusTokenStrategyCallback = async (
 	accessToken,
 	refreshToken,
 	profile,
 	next
-) =>
-	next(null, {
+) => {
+	const user = await getMongoRepository(User).findOne({
+		where: {
+			'googleplus._id': profile.id
+		}
+	})
+
+	if (user) {
+		next(null, user)
+	}
+
+	console.log(profile)
+
+	// add create User
+	// getMongoRepository(User).save(
+	// 	new User({
+	// 		googleplus: {
+	// 			_id: profile.id,
+	// 			email: profile.emails[0].value
+	// 		}
+	// 	})
+	// )
+
+	await next(null, {
 		accessToken,
 		refreshToken,
 		profile
 	})
+}
 
 passport.use(
 	new GooglePlusStrategy(
