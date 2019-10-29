34d931b6ebc3949132f0d81f1509373f30a814a7
chin trinh <trinhchinchin@gmail.com>
2019-10-29 20:49:31 +0700

    FIX

--- src/dump/index.ts
+++ src/dump/index.ts
@@ -2,7 +2,8 @@ import * as child from 'child_process'
 // mongorestore --host hostname.com --port 27017 --username user --password pass mongodump/db/
 
 const dump: child.ChildProcess = child.exec(
-	'mongodump -h @ds147420.mlab.com:47420 -d chnirt-nest -c users -u admin -p chnirt1803 -o /backup/mongodump',
+	// 'mkdir backup',
+	'mongodump -h @ds147420.mlab.com:47420 -d chnirt-nest -c users -u admin -p chnirt1803 -o /backup',
 	() => {
 		console.log('hihi')
 	}
